9976d51049c43900ee2f00c668f03170a195c8a8
unknown <sailavishnu12@gmail.com>
2025-09-15 00:55:04 +0530

    Add frontend files

--- Server.js
+++ Server.js
@@ -3,7 +3,17 @@ const cors = require("cors");
 const mysql = require("mysql2");
 const path = require('path');
 
+
 const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+
+
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
